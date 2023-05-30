0b18bc3754d0f7255fd66299c6667e686d4e9647
sykz42 <corentin.chepeau@gmail.com>
2023-05-30 15:00:47 +0200

    format and lint

--- packages/renderer/src/index.ts
+++ packages/renderer/src/index.ts
@@ -3,13 +3,13 @@ import App from '/@/App.vue';
 
 // Vuetify
 import 'vuetify/styles';
-import { createVuetify } from 'vuetify';
+import {createVuetify} from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
 
 const vuetify = createVuetify({
-    components,
-    directives,
-  });
+  components,
+  directives,
+});
 
 createApp(App).use(vuetify).mount('#app');
